d1e8bbd4b0f7ccdbdc877020bd98f1d070186015
K.-Michael Aye <kmichael.aye@gmail.com>
2019-10-29 17:47:40 +0000

    Add exmample notebook and more features
    * Both 4h blocks and full day data readers exist for all 5 items

--- pymcs/io.py
+++ pymcs/io.py
@@ -2,14 +2,18 @@ from io import StringIO
 from pathlib import Path
 
 import pandas as pd
+from tqdm import tqdm
+
+from .converters import add_datetime_column
 
 
 def read_list_to_df(data):
     missing = data[1].split()[1:]
     cols = data[2].split()
     buffer = StringIO(''.join(data[3:]))
-    df = pd.read_csv(buffer, sep='\s+', names=cols, na_values=missing)
+    df = pd.read_csv(buffer, sep=r'\s+', names=cols, na_values=missing)
     return df
+
 
 class L2Reader:
     def __init__(self, fname):
@@ -64,9 +68,15 @@ class L2Reader:
         return read_list_to_df(self.profiles_data)
 
 
+def cols_to_numeric(df):
+    for col in df.columns:
+        df[col] = pd.to_numeric(df[col], errors='ignore')
+
+
 class DBManager:
     dbroot = Path('/cabeus/data/mcs/level2')
     l2_product_string = 'post2d_v*'
+    items = ['header', 'rms', 'limb', 'nadir', 'profiles']
 
     def __init__(self, month=None, timestring=''):
         self.month = month
@@ -83,7 +93,9 @@ class DBManager:
     @property
     def header_data(self):
         path = self.dbroot / f"{self.month}/{self.month}_header.parquet"
-        return pd.read_parquet(path)
+        df = pd.read_parquet(path)
+        add_datetime_column(df)
+        return df
 
     def get_hour_subfiles(self, timestring=None):
         """Get list of files inside 4h block folder.
@@ -103,22 +115,26 @@ class DBManager:
         filelist = sorted(list(productfolder.glob('*.out')))
         return filelist
 
-    def _read_4h_block(self, item, timestring=None):
+    def _get_4h_block(self, item, timestring_or_path):
         """Read all data for a 4h block into a pd.DataFrame.
 
         Parameters
         ----------
-        timestring : str
-            Format yymmddhh
-        item: {'header, 'rms', 'nadir', 'limb', 'profiles}
+        timestring_or_path : str, pathlib.Path
+            String of format yymmddhh[0000] or Path to 4h block directory
+        item: {'header, 'rms', 'nadir', 'limb', 'profiles'}
             String selecting the data item of the L2 data file to be read.
 
         Returns
         -------
         pd.DataFrame
         """
-        if timestring is None:
-            timestring = self.timestring
+        try:
+            timestring = timestring_or_path.name
+        except AttributeError:
+            timestring = timestring_or_path
+
+        self.last_timestring = timestring
         filelist = self.get_hour_subfiles(timestring)
         if not filelist:
             raise FileNotFoundError(f'No files found for {timestring}.')
@@ -131,44 +147,84 @@ class DBManager:
             else:
                 bucket.append(data)
         df = pd.concat(bucket, ignore_index=True)
+        cols_to_numeric(df)
         return df
 
-    def read_4h_header(self, timestring=None):
-        return self._read_4h_block('header', timestring)
+    def get_4h_header(self, timestring):
+        return self._get_4h_block('header', timestring)
 
-    def read_4h_rms(self, timestring=None):
-        return self._read_4h_block('rms', timestring)
+    def get_4h_rms(self, timestring):
+        return self._get_4h_block('rms', timestring)
 
-    def read_4h_nadir(self, timestring=None):
-        return self._read_4h_block('nadir', timestring)
+    def get_4h_nadir(self, timestring):
+        return self._get_4h_block('nadir', timestring)
 
-    def read_4h_limb(self, timestring=None):
-        return self._read_4h_block('limb', timestring)
+    def get_4h_limb(self, timestring):
+        return self._get_4h_block('limb', timestring)
 
-    def read_4h_profiles(self, timestring=None):
-        return self._read_4h_block('profiles', timestring)
+    def get_4h_profiles(self, timestring):
+        return self._get_4h_block('profiles', timestring)
+
+    def get_available_4h_folders(self, daystring):
+        "Find available files for a day."
+        monthfolder = self.dbroot / daystring[:4]
+        return list(monthfolder.glob(f'{daystring}*'))
 
     def get_day(self, item, daystring):
-        monthfolder = self.dbroot / daystring[:4]
-        folderlist = list(monthfolder.glob(f'{daystring}*'))
+        """Get MCS data for a calendar day.
+
+        Parameters
+        ----------
+        item : {'header, 'rms', 'nadir', 'limb', 'profiles'}
+            String selecting the data item of the L2 data file to be read.
+        daystring : str
+            Day format string in format yymmdd
+
+        Returns
+        -------
+        pd.DataFrame
+            DataFrame with combined data from all MCS data for the given day.
+        """
+        folderlist = self.get_available_4h_folders(daystring)
         bucket = []
-        for folder in folderlist:
-            bucket.append(self._read_4h_block())
+        for folder in tqdm(folderlist):
+            bucket.append(self._get_4h_block(item, folder.name))
+        return pd.concat(bucket, ignore_index=True)
+
+    def get_day_header(self, daystring):
+        return self.get_day('header', daystring)
+
+    def get_day_rms(self, daystring):
+        return self.get_day('rms', daystring)
+
+    def get_day_limb(self, daystring):
+        return self.get_day('limb', daystring)
+
+    def get_day_nadir(self, daystring):
+        return self.get_day('nadir', daystring)
+
+    def get_day_profiles(self, daystring):
+        return self.get_day('profiles', daystring)
 
 
-
-
-def read_header_data(month):
+def get_header_data(month):
     """Read the header data into a pd.DataFrame
+
+    Returns pd.DataFrame with all data for the given month combined, all
+    data columns of the correct type and a datetime column as index (derived from
+    Date and Time columns) for automatic plotting over time.
 
     Parameters
     ----------
     month: str
-        String depicting the month of observations in the format {mmyy}
+        String selecting the month of observations in the format {mmyy}
 
     Returns
     -------
     pd.DataFrame
+       DataFrame with all header data for the given month combined (approx 45 MB)
+       and the index set to a full functional datetime object, for improved plotting
+       over time.
     """
     db = DBManager(month)
     return db.header_data
