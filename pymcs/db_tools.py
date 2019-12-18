b29fe0ecf1be2007b68191ed96ccba65d3e22e79
K.-Michael Aye <kmichael.aye@gmail.com>
2019-12-18 12:46:39 -0700

    Updating MCSDB class and notebook

--- pymcs/db_tools.py
+++ pymcs/db_tools.py
@@ -6,8 +6,9 @@ import pandas as pd
 
 class MCSDB:
     inifile = Path.home() / ".mcs_db.ini"
-    base_search = "select temperature from mcs_data_2d where "
-    example_dict = {"obsdate": 20070101, "obstime": 14486.727}
+    example_sql = """select temperature from mcs_data_2d
+    where obsdate = 20070101 and obstime = 14486.727
+    """
 
     def __init__(self):
         with self.inifile.open() as f:
@@ -15,9 +16,15 @@ class MCSDB:
         self.con = cx_Oracle.connect(self.url)
 
     def test_call(self):
-        return self.get_data_by_dict(self.example_dict)
+        return self.query(self.example_sql)
 
-    def send_sql(self, sql):
+    def get_columns(self):
+        sql = """select * from mcs_data_2d
+        where obsdate = 20070101 and obstime = 14486.727
+        """
+        return self.query(sql).columns
+
+    def query(self, sql):
         "most basic method, using pure SQL, returning pandas DataFrame."
         return pd.read_sql(sql, self.con)
 
@@ -29,16 +36,9 @@ class MCSDB:
         # cut off last 'and '
         return s[:-5]
 
-    def get_condition(self, cond):
-        "write your own condition string"
-        return self.send_sql(self.base_search + cond)
-
-    def get_data_by_dict(self, dict):
-        return self.get_condition(self.sqlize(dict))
-
     def get_cols_by_date(self, cols, datestr):
         sql = f"select {','.join(cols)} from mcs_data_2d where "
         sql += f"obsdate = {datestr}"
         print("Sending this request:")
         print(sql)
-        return self.send_sql(sql)
+        return self.query(sql)
