f16acc1385bd6b93a0e4026b5ab65683d65e52d0
K.-Michael Aye <kmichael.aye@gmail.com>
2020-01-01 19:14:42 -0700

    add more tests
    * test_get_MY_bracket
    * test_DateConverter_set_datetime
    * test_SQLizer
    * test_add_utc_col

--- tests/test_dbtools.py
+++ tests/test_dbtools.py
@@ -1,6 +1,9 @@
-from pymcs import db_tools
-import pytest
 from datetime import datetime as dt
+
+import pandas as pd
+import pytest
+
+from pymcs import db_tools
 
 
 @pytest.fixture
@@ -41,4 +44,40 @@ def test_dateconverter_mcsdate(mcsdate):
     assert pytest.approx(out_mcsdate[1]) == 14486.727
 
 
-def test_
+def test_get_MY_bracket():
+    t1, t2 = db_tools.get_MY_bracket(31)
+    assert t1 == "2011-09-13T19:12:00"
+    assert t2 == "2013-08-01T18:44:00"
+
+
+def test_DateConverter_set_datetime():
+    t1, t2 = db_tools.get_MY_bracket(31)
+    datecon = db_tools.DateConverter(utcdate=t1)
+    datecon.datetime = dt.fromisoformat(t2)
+    assert datecon.utcdate == "2013-08-01T18:44:00"
+
+
+def test_SQLizer():
+    sqlizer = db_tools.SQLizer(["obsdate", "obstime"], "profile", "obsdate = 20070101")
+    sqlizer.start_sql()
+    assert (
+        sqlizer.sql
+        == "select obsdate,obstime from MCS_PROFILE_DATA where obsdate = 20070101"
+    )
+
+
+def test_add_utc_col():
+    from io import StringIO
+
+    txt = """
+TNADIR	OBSTIME	OBSDATE	LS
+0	4	12776.435	20120617	125.92
+1	4	12807.154	20120617	125.92
+2	4	12837.874	20120617	125.92
+3	4	12868.594	20120617	125.92
+4	4	12899.313	20120617	125.92
+"""
+    df = pd.read_csv(StringIO(txt), sep=r"\s+")
+    newdf = db_tools.add_utc_col(df)
+    assert hasattr(newdf, "UTC")
+    assert newdf.UTC.iloc[0].isoformat() == "2012-06-17T03:32:56.434999"
